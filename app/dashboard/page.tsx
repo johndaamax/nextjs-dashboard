87c1a97b5daa4f42fea163b7f806584dee4406c8
johndaamax <ia.uoistud@gmail.com>
2024-02-04 15:29:45 +0100

    chapter 7: latestInvoices data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData, fetchRevenue } from '../lib/data';
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '../lib/data';
 import { Suspense } from 'react';
 
 export default async function Page() {
@@ -13,6 +13,7 @@ export default async function Page() {
     numberOfCustomers,
     numberOfInvoices,
   } = await fetchCardData();
+  const latestInvoices = await fetchLatestInvoices();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -32,7 +33,7 @@ export default async function Page() {
         <Suspense fallback={<div>Loading revenue...</div>}>
           <RevenueChart revenue={revenue} />
         </Suspense>
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
