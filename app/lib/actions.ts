cacd3b5578842a881b178a97b955a6bd919f1ee7
johndaamax <ia.uoistud@gmail.com>
2024-02-13 11:28:36 +0100

    chapter 12: server action - createInvoice

---
+++ app/lib/actions.ts
@@ -0,0 +1,39 @@
+'use server';
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  //validate the form data
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date();
+  const formattedDate = new Date(
+    date.getTime() - date.getTimezoneOffset() * 60000,
+  )
+    .toISOString()
+    .split('T')[0];
+
+  await sql`INSERT INTO invoices (customer_id, amount, status, date) VALUES (${customerId}, ${amountInCents}, ${status}, ${formattedDate})`;
+
+  //data changed on the route, revalidate client cached route
+  //Once the database has been updated, the /dashboard/invoices path will be revalidated, and fresh data will be fetched from the server.
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
